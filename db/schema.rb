62583543bcb115254cdd89f1526585303cd1cf8b
Arash Badie Modiri <arashbm@gmail.com>
2012-12-13 00:12:25 +0330

    filled field

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121207220636) do
+ActiveRecord::Schema.define(:version => 20121212150215) do
 
   create_table "admins", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -30,6 +30,14 @@ ActiveRecord::Schema.define(:version => 20121207220636) do
 
   add_index "admins", ["email"], :name => "index_admins_on_email", :unique => true
   add_index "admins", ["reset_password_token"], :name => "index_admins_on_reset_password_token", :unique => true
+
+  create_table "admins_forms", :force => true do |t|
+    t.string   "title"
+    t.boolean  "enabled"
+    t.text     "description"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
   create_table "blogs", :force => true do |t|
     t.string   "title"
@@ -50,6 +58,14 @@ ActiveRecord::Schema.define(:version => 20121207220636) do
   end
 
   add_index "fields", ["form_id"], :name => "index_fields_on_form_id"
+
+  create_table "filled_fields", :force => true do |t|
+    t.text     "value"
+    t.integer  "field_id"
+    t.integer  "filled_form_id"
+    t.datetime "created_at",     :null => false
+    t.datetime "updated_at",     :null => false
+  end
 
   create_table "filled_forms", :force => true do |t|
     t.integer  "form_id"
