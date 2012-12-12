dc6ba0b1ebb504aefa537a99314dd5eb1857c377
Arash Badie Modiri <arashbm@gmail.com>
2012-12-13 00:12:25 +0330

    filled field

--- app/models/filled_form.rb
+++ app/models/filled_form.rb
@@ -1,5 +1,13 @@
 class FilledForm < ActiveRecord::Base
   belongs_to :form
   belongs_to :user
-  # attr_accessible :title, :body
+  has_many :filled_fields
+  
+  after_initialize :ensure_fields, if: :persisted?
+
+  def ensure_fields
+    form.fields.each do |i|
+      filled_fields.find_or_create_by_field_id!(i.id)
+    end
+  end
 end
