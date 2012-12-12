dc1be186cc1dcf92157d7de22b288849efaeb327
Arash Badie Modiri <arashbm@gmail.com>
2012-12-13 01:30:44 +0330

    added ability for users to fill forms

--- app/models/filled_form.rb
+++ app/models/filled_form.rb
@@ -19,6 +19,10 @@ class FilledForm < ActiveRecord::Base
     self.state ||= :not_reviewed
   end
 
+  def state
+    'dummy'
+  end
+
   def ensure_fields
     form.fields.each do |i|
       filled_fields.find_or_create_by_field_id!(i.id)
