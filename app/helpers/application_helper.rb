def207ee1213b72b1da1fd8be30de6b160e612a6
Arash Badie Modiri <arashbm@gmail.com>
2012-12-15 03:15:02 +0330

    made a great navbar for anyone

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,12 @@
 module ApplicationHelper
+  def filled_form_navbar_item(form)
+    count = form.filled_forms.where( state: [nil, :not_reviewed] ).count
+    if count > 0
+      "#{h form.title} <i class='badge'>#{count}</i>".html_safe
+    else
+      form.title
+    end
+  end
   def flash_class(level)
     case level
     when :notice then "success"
