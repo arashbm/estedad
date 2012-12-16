cafd345b14b7739bcb51e5363d2681e705be3ffd
Arash Badie Modiri <arashbm@gmail.com>
2012-12-16 15:34:08 +0330

    added name to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121215061014) do
+ActiveRecord::Schema.define(:version => 20121216072256) do
 
   create_table "admins", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
