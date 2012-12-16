6afd809f7a56b3cad5269bbbd6e786f3de1a598c
Arash Badie Modiri <arashbm@gmail.com>
2012-12-16 15:34:08 +0330

    added name to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,11 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me,
+    :first_name, :last_name
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
   
   has_many :filled_forms, order: "updated_at DESC"
 
@@ -27,7 +30,6 @@ class User < ActiveRecord::Base
   end
 
   def name
-    # placeholder for actual field
-    email
+    "#{first_name} #{last_name}"
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,7 @@
 # It's strongly recommended to check this file into your version control system.
 
 ActiveRecord::Schema.define(:version => 20121216072256) do
+
   create_table "admins", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
     t.string   "encrypted_password",     :default => "", :null => false
