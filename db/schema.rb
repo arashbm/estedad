c478d493de897d4e08781aeab5bcde1e5af4f1cb
Arash Badie Modiri <arashbm@gmail.com>
2012-12-17 00:12:17 +0330

    remade schema after rebasing

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121216072256) do
+ActiveRecord::Schema.define(:version => 20121215061014) do
 
   create_table "admins", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -80,6 +80,7 @@ ActiveRecord::Schema.define(:version => 20121216072256) do
     t.integer  "user_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.text     "data"
     t.string   "state"
   end
 
@@ -107,8 +108,6 @@ ActiveRecord::Schema.define(:version => 20121216072256) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
-    t.string   "first_name"
-    t.string   "last_name"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
