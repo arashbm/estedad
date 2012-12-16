394e660ac9338a3468caeadb973104076694cb89
Arash Badie Modiri <arashbm@gmail.com>
2012-12-16 15:16:42 +0330

    Rebased to develope

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,6 @@
 # It's strongly recommended to check this file into your version control system.
 
 ActiveRecord::Schema.define(:version => 20121216072256) do
-
   create_table "admins", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
     t.string   "encrypted_password",     :default => "", :null => false
@@ -80,6 +79,7 @@ ActiveRecord::Schema.define(:version => 20121216072256) do
     t.integer  "user_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.text     "data"
     t.string   "state"
   end
 
